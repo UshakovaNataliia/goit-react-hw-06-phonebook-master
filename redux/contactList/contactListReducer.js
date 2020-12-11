3cbf0f2826f3bce6339dee4e8a524c21668a9462
UshakovaNataliia <65922563+UshakovaNataliia@users.noreply.github.com>
2020-12-11 21:30:53 +0200

    +

--- redux/contactList/contactListReducer.js
+++
@@ -1,8 +0,0 @@
-import { addContactItem, deleteContact, setContacts} from './contactListActions';
-import {createReducer} from '@reduxjs/toolkit';
-
-export const contacts = createReducer([], {
-    [addContactItem]: (state, action) => [...state, action.payload.contact],
-    [deleteContact]: (state, action) => state.filter(contact => contact.id !== action.payload),
-    [setContacts]: (state, action) => [...state, ...action.payload],
-});
--- redux/filter/filterReducer.js
+++
@@ -1,6 +0,0 @@
-import { filterInput } from './filterActions';
-import { createReducer } from '@reduxjs/toolkit';
-
-export const filter = createReducer('', {
-    [filterInput]: (_, action) => action.payload, // _ - state не используется
-});
